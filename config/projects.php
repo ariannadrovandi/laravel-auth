8a8b8e42234ea78584cdc9f452a4edbc61d3b72f
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-06-07 18:46:33 +0200

    fix

--- config/projects.php
+++ config/projects.php
@@ -2,33 +2,39 @@
 return [
     [
         'title' => 'laravel-auth',
+        'image' => 'https://res.cloudinary.com/sujipthapa/image/fetch/q_auto,f_auto/https://sujipthapa.co/media/45/1514804841.login.png',
         'description' => "Un sito Web che ti consente di effettuare il login per aprire la dashboard di amministrazione utilizzando Laravel",
-        'image' => 'https://res.cloudinary.com/sujipthapa/image/fetch/q_auto,f_auto/https://sujipthapa.co/media/45/1514804841.login.png'
+
     ],
     [
         'title' => 'laravel-dc-comics',
+        'image' => 'https://i.pcmag.com/imagery/reviews/02GxH5SrosYG4f1JyKLdghB-1..v1584738189.png',
         'description' => 'Un sito Web che ti consente di visualizzare un elenco di fumetti DC utilizzando Laravel ',
-        'image' => 'https://i.pcmag.com/imagery/reviews/02GxH5SrosYG4f1JyKLdghB-1..v1584738189.png'
+
     ],
     [
         'title' => 'laravel-comics',
+        'image' => 'https://images.everyeye.it/img-notizie/panini-annuncia-fumetti-dc-comics-arrivo-lista-completa-v3-426985.jpg',
         'description' => 'Un sito Web che ti consente di visualizzare un elenco di fumetti DC utilizzando Laravel ',
-        'image' => 'https://images.everyeye.it/img-notizie/panini-annuncia-fumetti-dc-comics-arrivo-lista-completa-v3-426985.jpg'
+
     ],
     [
         'title' => 'laravel-migration-seeder',
+        'image' => 'https://lavalite.org/image/original/blog/blog/2018/11/09/071430363/images/laravel-migration.jpg',
         'description' => 'Un sito Web che ti consente di migrare un database utilizzando Laravel',
-        'image' => 'https://lavalite.org/image/original/blog/blog/2018/11/09/071430363/images/laravel-migration.jpg'
+
     ],
     [
         'title' => 'laravel-model-controller',
+        'image' => 'https://www.cloudways.com/blog/wp-content/uploads/Models-and-Views.jpg',
         'description' => 'Un sito Web che ti consente di creare un Model e un Controller utilizzando Laravel',
-        'image' => 'https://www.cloudways.com/blog/wp-content/uploads/Models-and-Views.jpg'
+
     ],
     [
         'title' => 'laravel-primi-passi',
+        'image' => 'https://www.filepicker.io/api/file/6TobjLBvQSOSYdjD4NTL',
         'description' => 'Un sito Web che mostra i primi passi nel mondo di Laravel',
-        'image' => 'https://www.filepicker.io/api/file/6TobjLBvQSOSYdjD4NTL'
+
     ],
 
 ];
